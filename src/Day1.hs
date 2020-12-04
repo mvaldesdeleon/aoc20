d6b8ced636df94482ae82d28208bf169a6c8fd30
Martín Valdés de León <mvdl@amazon.de>
2020-12-04 23:39:04 +0100

    feat: day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -3,12 +3,32 @@ module Day1
   )
 where
 
+import Data.List (nub)
 import Paths_aoc20 (getDataFileName)
 
 loadInput :: IO String
 loadInput = getDataFileName "inputs/day-1.txt" >>= readFile
 
+parseInput :: String -> [Integer]
+parseInput = map read . lines
+
+find2020 :: [Integer] -> Maybe (Integer, Integer)
+find2020 [] = Nothing
+find2020 (first : rest) =
+  case filter (\second -> second + first == 2020) rest of
+    [] -> find2020 rest
+    (second : _) -> Just (first, second)
+
+find2020' :: [Integer] -> Maybe (Integer, Integer, Integer)
+find2020' nums =
+  case [(x, y, z) | x <- nums, y <- nums, z <- nums, x + y + z == 2020] of
+    [] -> Nothing
+    ((x, y, z) : _) -> Just (x, y, z)
+
 day1 :: IO ()
 day1 = do
-  input <- loadInput
-  print $ "hello world - " ++ input
+  input <- parseInput <$> loadInput
+  let Just (first, second) = find2020 input
+  print $ first * second
+  let Just (first', second', third') = find2020' input
+  print $ first' * second' * third'
