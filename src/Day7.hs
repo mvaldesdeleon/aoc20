c42cd3acea246bcad0decef3c30c4ab338d70daa
Martín Valdés de León <mvdl@amazon.de>
2020-12-20 19:55:47 +0100

    feat: day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -5,11 +5,13 @@ where
 
 import Control.Applicative ((<|>))
 import Data.Functor (($>))
+import qualified Data.List as L
+import qualified Data.Map.Strict as M
 import Paths_aoc20 (getDataFileName)
 import qualified Text.Parsec as P
 
 newtype Bag = Bag String
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 data Rule = Rule
   { rBag :: Bag,
@@ -38,7 +40,7 @@ parseContents = P.try (P.string "no other bags" $> []) <|> (parseContent `P.sepB
       bag <- parseBag
       P.string " bag"
       P.optional $ P.char 's'
-      return $ (count, bag)
+      return (count, bag)
 
 parseRule :: P.Parsec String () Rule
 parseRule = do
@@ -54,7 +56,36 @@ parseInput input =
     Right rules -> rules
     Left err -> error $ show err
 
+isContainedInMap :: [Rule] -> M.Map Bag [Bag]
+isContainedInMap = go M.empty
+  where
+    go map [] = map
+    go map (r : rs) = go (updateMap r map) rs
+    updateMap Rule {rBag = bigBag, rContents = contents} map =
+      foldl (updateContent bigBag) map contents
+    updateContent bigBag map (_, smallBag) = M.alter (setOrInsert bigBag) smallBag map
+    setOrInsert bigBag Nothing = Just [bigBag]
+    setOrInsert bigBag (Just rest) = Just (bigBag : rest)
+
+findBags :: M.Map Bag [Bag] -> Bag -> [Bag]
+findBags isContainedIn bag =
+  let bigBags = M.findWithDefault [] bag isContainedIn
+   in bigBags ++ concat (findBags isContainedIn <$> bigBags)
+
+containsMap :: [Rule] -> M.Map Bag [(Integer, Bag)]
+containsMap = go M.empty
+  where
+    go map [] = map
+    go map (Rule {rBag = bag, rContents = contents} : rs) = go (M.insert bag contents map) rs
+
+countBags :: M.Map Bag [(Integer, Bag)] -> Bag -> Integer
+countBags contains bag = sum $ countBagsOfType <$> contains M.! bag
+  where
+    countBagsOfType (count, bag) = count + (count * countBags contains bag)
+
 day7 :: IO ()
 day7 = do
   input <- parseInput <$> loadInput
-  print $ input
+  let shinyGold = Bag "shiny gold"
+  print $ length . L.nub $ findBags (isContainedInMap input) shinyGold
+  print $ countBags (containsMap input) shinyGold
