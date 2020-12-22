7d809150661249b7674db7920d050699e317c882
Martín Valdés de León <mvdl@amazon.de>
2020-12-23 00:52:32 +0100

    feat: day 13 part 2

--- src/Day13.hs
+++ src/Day13.hs
@@ -41,21 +41,33 @@ busAndDeltas Input {iTimestamp = timestamp, iBuses = buses} = catPairs $ zip bus
     catPairs ((Nothing, _) : rest) = catPairs rest
     catPairs ((Just id, idx) : rest) = (id, idx) : catPairs rest
 
-xxx :: Input -> Integer
-xxx input =
-  let bad = busAndDeltas input
-      (id, delta) = L.maximumBy (compare `on` fst) bad
-   in go bad id delta (100000000000000 `div` id)
+solveContest :: Input -> Integer
+solveContest input =
+  let congruenceSystem = map xx $ busAndDeltas input
+   in fst $ crt congruenceSystem
   where
-    go bad id delta n =
-      let t = n * id - delta
-       in if match bad t
-            then t
-            else go bad id delta (n + 1)
-    match bad t = all (\(id, delta) -> (t + delta) `mod` id == 0) bad
+    xx (bus, delta) = (if delta > 0 then bus - delta else 0, bus)
+
+-- Shamelessly stolen from StackOverflow: https://stackoverflow.com/a/35529381
+crt :: (Integral a, Foldable t) => t (a, a) -> (a, a)
+crt = foldr go (0, 1)
+  where
+    go (r1, m1) (r2, m2) = (r `mod` m, m)
+      where
+        r = r2 + m2 * (r1 - r2) * (m2 `inv` m1)
+        m = m2 * m1
+
+    -- Modular Inverse
+    a `inv` m = let (_, i, _) = gcd a m in i `mod` m
+
+    -- Extended Euclidean Algorithm
+    gcd 0 b = (b, 0, 1)
+    gcd a b = (g, t - (b `div` a) * s, s)
+      where
+        (g, s, t) = gcd (b `mod` a) a
 
 day13 :: IO ()
 day13 = do
   input <- parseInput <$> loadInput
   print $ uncurry (*) $ nextBus input
-  print $ xxx input
+  print $ solveContest input
